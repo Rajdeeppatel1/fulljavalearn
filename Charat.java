21f03ee6b112f0f881b81b1e182dd3cd09da3b4f
Rajdeep patel <patelrajdeep537@gmail.com>
2026-09-23 22:09:07 +0530

    charat

---
+++ Charat.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Charat {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+    String a="hiut";
+    System.out.println(a.charAt(3));
+    }
+    
+}
